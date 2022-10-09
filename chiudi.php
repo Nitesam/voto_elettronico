634c93bc35dbff355e9581e39672278a1f6ff5d2
dcocuzza <dcocuzza01@gmail.com>
2022-10-09 12:17:12 +0200

    aggiunto spazio

--- chiudi.php
+++ chiudi.php
@@ -3,11 +3,13 @@ session_start();
 ?>
 
 <html>
+
 <body>
     <?php
     session_unset();
     session_destroy();
-    echo("<script type='text/javascript'> alert('Sessione Chiusa con Successo!'); window.location.replace(\"login_registra/primo.php\"); </script>");
+    echo ("<script type='text/javascript'> alert('Sessione Chiusa con Successo!'); window.location.replace(\"login_registra/primo.php\"); </script>");
     ?>
+
 </html>
 </body>
